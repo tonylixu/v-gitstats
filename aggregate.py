b8d47475f9e096684b2ceb696dce4952b98dfb75
tonylixu@gmail.com <tonylixu@gmail.com>
2017-08-09 13:57:18 -0400

    Added generateAggregate function

--- aggregate.py
+++ aggregate.py
@@ -7,3 +7,15 @@ import csv, os, re, sys
 # the maximum field size.
 # sys.maxsize: The largest positive integer supported by the platform.
 csv.field_size_limit(sys.maxsize)
+
+def generateAggregate(arrToTSV):
+    """Generate and returns JSON data for the 'Aggregate' page for a
+    given project.
+
+    Arguments:
+        arrToTSV { [str] }: relative paths to the 'general.tsv' for each sub-project under
+    a main project, ex: ['/static/data/TUT/DataWedge/general.tsv','/static/data/TUT/ardvarc/general.tsv']
+
+    Return:
+        dict of data ready for 'Aggregate' page
+    """
